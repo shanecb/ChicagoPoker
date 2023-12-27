0a053b32153ed503a4f569c4ce3f17398b6caa99
Shane Bielefeld <shanebielefeld@gmail.com>
2023-12-27 11:49:57 -0500

    Initial Commit

---
+++ ChicagoPoker/ChicagoPokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChicagoPokerApp.swift
+//  ChicagoPoker
+//
+//  Created by Shane Bielefeld on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ChicagoPokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
